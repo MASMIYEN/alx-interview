0a6219337690ea3f4a181bf681a9d4be2fd2fce1
MASMIYEN <masmiyen@gmail.com>
2024-04-09 05:36:16 +0000

    pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 pascal_triangle.py
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers
@@ -16,7 +17,7 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
 
