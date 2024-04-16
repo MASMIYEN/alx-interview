6da279e5b0343d99e99b64c23980438d0fe30567
MASMIYEN <masmiyen@gmail.com>
2024-04-16 12:48:03 +0100

    forgot shbang

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,27 @@
+#!/usr/bin/python3
+"""BOXES BOXES"""
+
 def canUnlockAll(boxes):
     """
-    take boxes
-        create set of keys
-            go to box0
-                get all keys and add them setofkeys
-            start opening boxes from setofkeys
-                go to each box of each key
-                    and take the keys from it and add them to set of keys
-                keep loping through all setof keys
-            ignore keys that dont have box
-            track opening of boxes by a counter, if at end it
-            equal to lentgh of boxes it mean all boxes unlock
-            OPTIMIZE IDEA :
-                if we add 0 to setofkeys at start, we dont need for in 23
+    This function determines if all boxes can be unlocked by following a set of keys.
+
+    The function operates as follows:
+    1. It initializes a set of keys with the key from the first box (box0).
+    2. It then iterates through the keys in the set, using each key to unlock a box.
+    3. For each unlocked box, it retrieves all keys from that box and adds them to the set of keys to unlock.
+    4. This process continues until no new keys can be added to the set, indicating that all boxes have been unlocked.
+    5. The function tracks the number of boxes unlocked and compares it to the total number of boxes. If the count matches the total, it means all boxes have been successfully unlocked.
+
+    Parameters:
+        boxes (list): A list of lists, where each sublist represents a box and contains the keys that can unlock it.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+
+    Note:
+        - The function assumes that the first box (box0) can be unlocked with the key 0.
+        - Keys that do not correspond to a box are ignored.
+        - The optimization idea mentioned (adding 0 to setofkeys at start) is already implemented in the function's logic.
     """
     total_boxes = len(boxes)
     setofkeys = [0]
